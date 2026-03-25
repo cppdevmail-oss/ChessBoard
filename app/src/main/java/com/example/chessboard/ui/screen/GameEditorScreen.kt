7d46b9003abb023c36f4d7b0fa138576eb1d7c2e
lokis <cppdevmail@gmail.com>
2026-03-25 11:53:24 +0100

    minor fix

--- app/src/main/java/com/example/chessboard/ui/screen/GameEditorScreen.kt
+++ app/src/main/java/com/example/chessboard/ui/screen/GameEditorScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.example.chessboard.boardmodel.GameController
