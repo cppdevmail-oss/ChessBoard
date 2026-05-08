16a84afc238f37cc42032c38ecea0d1e57a05793
lokis <cppdevmail@gmail.com>
2026-05-08 08:22:11 +0200

    move to package file for castles

--- app/src/main/java/com/example/chessboard/ui/screen/positions/positionSearch/PositionSearchCastles.kt
+++ app/src/main/java/com/example/chessboard/ui/screen/positions/positionSearch/PositionSearchCastles.kt
@@ -1,4 +1,4 @@
-package com.example.chessboard.ui.screen
+package com.example.chessboard.ui.screen.positions.positionSearch
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
